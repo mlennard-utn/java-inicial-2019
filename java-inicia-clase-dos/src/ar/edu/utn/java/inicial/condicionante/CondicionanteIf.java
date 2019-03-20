397b4227384a11ac2d8a0582e289bd049dd26a5d
alex de assis <alexdeassis@gmail.com>
2019-03-20 20:56:15 -0300

    Comentario primeras clases

---
+++ java-inicia-clase-dos/src/ar/edu/utn/java/inicial/condicionante/CondicionanteIf.java
@@ -0,0 +1,19 @@
+package ar.edu.utn.java.inicial.condicionante;
+
+public class CondicionanteIf {
+	
+	public static void main(String[] args) {
+		
+		int valor = 1;
+		
+		if(valor < 0) {
+			// bloque de codigo si se cumple
+			// la condicion
+			System.out.println("Valor es negativo");
+		} else {
+			System.out.println("Valor es positivo");
+		}
+		
+	}
+	
+}
---
+++ java-inicial-clase-uno/src/ar/edu/utn/java/programa/PrimerPrograma.java
@@ -0,0 +1,10 @@
+package ar.edu.utn.java.programa;
+
+public class PrimerPrograma {
+	
+	public static void main(String[] args) {
+		System.out.println("Hola mundo!");
+		System.out.println("Chau mundo!");
+	}
+	
+}
